2012bb90449c80e1c57a779090b993f904d40a18
trotrem <dgici@hotmail.com>
2017-11-25 10:49:34 -0500

    cleaned up stuff and fixed layout

--- src/components/ComicPage.jsx
+++ src/components/ComicPage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components";
 import { media } from "../static/js/style-utils.js";
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 import Scroller from "./Scroller.jsx";
 import Sticky from "react-stickynode";
 import NavBar from "./NavBar.jsx";
-import glamorous from 'glamorous';
-import background from '../static/img/background.png';
-import banner from '../static/img/banner.png';
+import glamorous from "glamorous";
+import background from "../static/img/background.png";
+import banner from "../static/img/banner.png";
 
 const Grid = styled.div`
   display: grid;
@@ -17,68 +17,63 @@ const Grid = styled.div`
   background-attachment: fixed;
 `;
 
-const Comic = styled.div`
-  grid-area: comic;
-  flex: 1 0 auto;
-  display: flex;
-  flex-direction: column;
-  overflow: auto;
-`;
+const ComicContainer = glamorous.div(
+  {
+    gridArea: "content",
+    width:"100%",
+    height:"100%"
+  }
+);
 
-const LeftMargin = styled.div`
-  height: 200vh;
-  ${media.tablet`
-      display:none;
-  `};
-`;
+const Comic = glamorous.div(
+  ({ pageWidth }) => ({
+    maxWidth: pageWidth
+  }),
+  {
+    display: "flex",
+    flexDirection: "column",
+    flex: "1 1 auto",
+    overflow: "auto",
+    margin: "auto"
+  }
+);
 
-const RightMargin = styled.div`
-  height: 200vh;
-  ${media.tablet`
-      display:none;
-  `};
-`;
 const TopBanner = styled.div`
   grid-area: banner;
   background-image: url(${banner});
   background-position: center;
 `;
 
-const ComicContainer = glamorous.div(
-  ({pageWidth}) => ({
-    gridTemplateColumns: '1fr ' + pageWidth + 'px 1fr',
-  }),
-  {
-    gridArea: 'content',
-    display: 'grid',
-    gridTemplateAreas: `"left comic right"`,
-    '@media(max-width: 750px)': {
-      gridTemplateColumns: '1fr',
-      gridTemplateAreas: `"comic"`
-    }
-  }
+const NavSticky = styled(Sticky)`
+  grid-area: navigation;
+`;
+
+const scrollerRenderer = (resolution, page) => (
+  <Scroller pageNum={page} resolution={resolution} />
 );
 
-const scrollerRenderer = (resolution, page) => <Scroller pageNum={page} resolution={resolution}/>;
-
-const ComicPage = (props) => (
+const ComicPage = props => (
   <Grid>
-    <TopBanner/>
-      <ComicContainer pageWidth={props.resolution[0]}>
-        <Sticky>
-          <LeftMargin/>
-        </Sticky>
-        <Comic>
-          <Route exact={true} path="/mandy" render={() => scrollerRenderer(props.resolution, 0)}/>
-          <Route exact={true} path="/mandy/:pageNum" render={({match}) => scrollerRenderer(props.resolution, match.params.pageNum)}/>
-        </Comic>
-        <Sticky>
-          <RightMargin/>
-        </Sticky>
-      </ComicContainer>
-      <Sticky>
-        <NavBar/>
-      </Sticky>
+    <TopBanner />
+    <ComicContainer>
+    <Comic pageWidth={props.resolution[0]}>
+      <Route
+        exact={true}
+        path="/mandy"
+        render={() => scrollerRenderer(props.resolution, 0)}
+      />
+      <Route
+        exact={true}
+        path="/mandy/:pageNum"
+        render={({ match }) =>
+          scrollerRenderer(props.resolution, match.params.pageNum)
+        }
+      />
+    </Comic>
+    </ComicContainer>
+    <NavSticky>
+      <NavBar />
+    </NavSticky>
   </Grid>
 );
 
--- src/components/Scroller.jsx
+++ src/components/Scroller.jsx
@@ -1,57 +1,61 @@
 import React from "react";
 import styled from "styled-components";
 import { List, AutoSizer, WindowScroller } from "react-virtualized";
-import styles from '../static/css/styles.css'
+import styles from "../static/css/styles.css";
 
 const ComicPage = styled.img`
-max-width: 100%;
-border: 10px solid black;
+  max-width: 100%;
+  border: 10px solid black;
 `;
 
 const rowHeight = (width, pageWidth, borderSize) => {
-    return Math.min(pageWidth * ((990 + borderSize)/765), width * ((990 + borderSize)/765));
+  return Math.min(
+    pageWidth * ((990 + borderSize) / 765),
+    width * ((990 + borderSize) / 765)
+  );
 };
 
 function rowRenderer(folder) {
-  return function({
-    key,
-    index,
-    style
-  }){
+  return function({ key, index, style }) {
     return (
       <div key={key} style={style}>
-        <ComicPage src={require("../static/img/" + folder + "/" + (index % 28 + 1) + ".png")} />
+        <ComicPage
+          src={require("../static/img/" +
+            folder +
+            "/" +
+            (index % 28 + 1) +
+            ".png")}
+        />
       </div>
     );
-  }
+  };
 }
 
-const Scroller = (props) => {
+const Scroller = props => {
   return (
-    <div className={styles.WindowScrollerWrapper}>
-      <WindowScroller>
-        {({ height, isScrolling, onChildScroll, scrollTop }) => (
-          <AutoSizer disableHeight>
-            {({ width }) => {
-            return(
+    <WindowScroller>
+      {({ height, isScrolling, onChildScroll, scrollTop }) => (
+        <AutoSizer disableHeight>
+          {({ width }) => {
+            return (
               <List
                 autoHeight
                 height={height}
                 isScrolling={isScrolling}
                 onScroll={onChildScroll}
                 rowCount={28}
-                rowHeight={rowHeight(width, props.resolution[0], 0)}
+                rowHeight={rowHeight(width, props.resolution[0], 10)}
                 rowRenderer={rowRenderer(props.resolution[2])}
                 scrollTop={scrollTop}
                 width={width}
                 scrollToIndex={props.pageNum - 1}
                 scrollToAlignment="start"
               />
-            )}}
-          </AutoSizer>
-        )}
-      </WindowScroller>
-    </div>
+            );
+          }}
+        </AutoSizer>
+      )}
+    </WindowScroller>
   );
 };
 
