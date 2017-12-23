13f771720062f8223f72111ea927f921a9c8588a
trotrem <dgici@hotmail.com>
2017-12-22 22:23:16 -0500

    new parallax images

--- src/components/scenes/mainPage/Main.jsx
+++ src/components/scenes/mainPage/Main.jsx
@@ -1,43 +1,41 @@
 import React from "react";
-import styled from "styled-components";
+import glamorous from "glamorous";
 import Plx from "../../common/Parallax";
 import ShelfMenu from "./ShelfMenu.jsx"
 
-const Container = styled.div`
-  position: relative;
-  background-color: #898989;
-  height: 200vh;
-  overflow: hidden;
-`;
+const layerPath = "./img/plxLayers/";
 
-const Tontainer = styled.div`
-  position: absolute;
-  top: 1080px;
-  left: 0px;
-  background-color: #898ff9;
-  height: calc(100% - 1080px);
-  width:100%;
-`;
+const imageHeight = 3839;
 
-const StyledLayer = styled.div`
-  position: absolute;
-  top: 0px;
-  left: 0px;
-  height: 1080px;
-  width:100%;
-`;
+const Container = glamorous.div(
+  {
+  position: "relative",
+  backgroundColor: "#898989",
+  height: "500vh",
+  overflow: "hidden"
+  }
+);
 
-const layerPath = "./img/plxLayers/compressed/";
+const Tontainer = glamorous.div(
+  {
+  position: "absolute",
+  top: imageHeight.toString(),
+  left: "0px",
+  backgroundColor: "#898ff9",
+  height: "calc(100% - " + imageHeight + "px)",
+  width: "100%"
+  }
+);
 
 const MainPage = props => (
   <Container >
-    <Plx image={require(layerPath + "1.png")} strength={700} height={1080}/>
-    <Plx image={require(layerPath + "2.png")} strength={600} height={1080}/>
-    <Plx image={require(layerPath + "3.png")} strength={300} height={1080}/>
-    <Plx image={require(layerPath + "4.png")} strength={200} height={1080}/>
-    <Plx image={require(layerPath + "5.png")} strength={100} height={1080}/>
-    <Plx image={require(layerPath + "6.png")} strength={0} height={1080}/>
-    <Plx image={require(layerPath + "7.png")} strength={-30} height={1080}/>
+    <Plx image={require(layerPath + "1.png")} strength={700} height={imageHeight}/>
+    <Plx image={require(layerPath + "2.png")} strength={600} height={imageHeight}/>
+    <Plx image={require(layerPath + "3.png")} strength={300} height={imageHeight}/>
+    <Plx image={require(layerPath + "4.png")} strength={200} height={imageHeight}/>
+    <Plx image={require(layerPath + "5.png")} strength={100} height={imageHeight}/>
+    <Plx image={require(layerPath + "6.png")} strength={0}   height={imageHeight}/>
+    <Plx image={require(layerPath + "7.png")} strength={-30} height={imageHeight}/>
     <Tontainer>
       <ShelfMenu/>
     </Tontainer>
