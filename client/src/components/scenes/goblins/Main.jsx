6ffd5f15cc6ffc27e7d82a3c8c9a34c200eace69
Jean-Christophe Buteau <jeanchristophe.buteau@gmail.com>
2018-05-12 17:28:24 -0400

    added buy and download buttons to layout

--- client/src/components/scenes/goblins/Main.jsx
+++ client/src/components/scenes/goblins/Main.jsx
@@ -41,12 +41,60 @@ const Scroll = styled("img")`
     width: 70%;
 `;
 
+const Stump = styled("img")`
+  position: absolute;
+  width: 100%;
+  bottom: 0px;
+`
+
+const Content = styled("div")`
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translate(-50%, 0);
+  width: 60%;
+  height: 100%;
+`;
+
+const PreviewFormats = styled("div")`
+  position: relative;
+  top: 40%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const FormatImage = styled("img")`
+  width: 18%;
+`;
+
+const ClickToDownload = styled("img")`
+  width: 40%;
+`;
+
+const BuyButton = styled("img")`
+  position: relative;
+  top: 50%;
+  width: 70%;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+`
+
 const MainPage = props => (
     <Container>
       <BlueGradient>
         <Scroll src={require(imgPath + "Scroll.png")}></Scroll>
-        <Frame>
-        </Frame>
+        <Stump src={require(imgPath + "Stump.png")}/>
+        <Content>
+          <PreviewFormats>
+            <FormatImage src={require(imgPath + "epub.png")}/>
+            <ClickToDownload src={require(imgPath + "ctd.png")}/>
+            <FormatImage src={require(imgPath + "kindle.png")}/>
+          </PreviewFormats>
+          <BuyButton src={require(imgPath + "buy.png")}/>
+        </Content>
+        <Frame/>
       </BlueGradient>
     </Container>
 );
