4b241350199b56d11f2798642253d4533c3aef57
Jean-Christophe Buteau <jeanchristophe.buteau@gmail.com>
2019-04-26 12:29:42 -0400

    fixed art images links

--- client/src/components/scenes/art/Main.jsx
+++ client/src/components/scenes/art/Main.jsx
@@ -3,61 +3,62 @@ import Gallery from "react-photo-gallery";
 import GalleryImage from "./GalleryImage.jsx";
 import styled from "react-emotion";
 
+let img_path = "./gallery_images/"
 const photos = [
   {
-    src: "https://themirandachick.deviantart.com/art/Master-644795410",
-    width: 600,
-    height: 898
+    src: require(img_path + "Cosmodaunt.jpg"),
+    width: 564,
+    height: 901
   },
   {
-    src: "https://img00.deviantart.net/2d99/i/2016/299/e/d/inktober_25___weight_of_splendor_by_themirandachick-dambgav.png",
+    src: require(img_path + "Crossfire.jpg"),
+    width: 564,
+    height: 717
+  },
+  {
+    src: require(img_path + "Enter_Darkness.jpg"),
+    width: 564,
+    height: 782
+  },
+  {
+    src: require(img_path + "Ghosty.jpg"),
+    width: 563,
+    height: 594
+  },
+  {
+    src: require(img_path + "Joker.jpg"),
+    width: 564,
+    height: 870
+  },
+  {
+    src: require(img_path + "Lazy_Dragon.jpg"),
+    width: 563,
+    height: 600
+  },
+  {
+    src: require(img_path + "master.png"),
+    width: 600,
+    height:898 
+  },
+  {
+    src: require(img_path + "Solar_Workings.jpg"),
+    width: 563,
+    height: 581
+  },
+  {
+    src: require(img_path + "Sword_in_Stone.jpg"),
+    width: 563,
+    height: 598
+  },
+  {
+    src: require(img_path + "Waiting_for_the_Next_Round.jpg"),
+    width: 564,
+    height: 686
+  },
+  {
+    src: require(img_path + "Weight_of_Splendour.jpg"),
     width: 400,
     height: 573
-  },
-  {
-    src: "https://img00.deviantart.net/e68d/i/2016/325/0/0/crossfire_by_themirandachick-daoxyfu.png",
-    width: 600,
-    height: 763
-  },
-  {
-    src: "https://pre00.deviantart.net/ad30/th/pre/f/2017/275/b/9/joker_by_themirandachick-dbpb3es.png",
-    width: 497,
-    height: 768
-  },
-  {
-    src: "https://img00.deviantart.net/c83e/i/2016/328/5/9/enter_darkness_by_themirandachick-dapeigs.png",
-    width: 600,
-    height: 832
-  },
-  {
-    src: "https://img00.deviantart.net/0b5e/i/2016/301/5/6/inktober_27___the_bigger_bite_by_themirandachick-damk8lj.png",
-    width: 600,
-    height: 610
-  },
-  {
-    src: "https://img00.deviantart.net/05c8/i/2018/016/9/4/space_mercenary_by_themirandachick-dc07mgo.png",
-    width: 800,
-    height: 1083
-  },
-  {
-    src: "https://img00.deviantart.net/65f2/i/2016/352/8/d/mandy_by_themirandachick-das1qyt.png",
-    width: 600,
-    height: 734
-  },
-  {
-    src: "https://img00.deviantart.net/0704/i/2016/339/1/2/waiting_for_the_next_round_by_themirandachick-daqepgp.png",
-    width: 600,
-    height: 730
-  },
-  {
-    src: "https://img00.deviantart.net/be9a/i/2016/313/c/a/solar_workings_by_themirandachick-danv8cx.png",
-    width: 600,
-    height: 619
-  },
-  {
-    src: "https://img00.deviantart.net/c1fa/i/2016/323/7/9/cosmodaunt_by_themirandachick-daou4yw.png",
-    width: 600,
-    height: 959
   }
 ];
 
